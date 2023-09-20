e1de6fae7cc5f59b9b60cf388eb0a38627ed0dee
Jimboss696 <sj.piruchkung@uea.edu.ec>
2023-09-19 22:39:23 -0500

    seman 11 trabajo2

--- Iteración de arreglos multidimensionales con bucles anidados.py
+++ Iteración de arreglos multidimensionales con bucles anidados.py
@@ -1,63 +1,59 @@
-# Paso 1: Crear una matriz 3D con datos de temperaturas
-# Supongamos que tenemos tres ciudades, siete días de la semana y cuatro semanas.
-# Puedes inicializar la matriz con datos de temperaturas ficticias.
-
-# Matriz 3D [ciudad][día de la semana][semana]
+# Matriz 3D [ciudad][semana][día de la semana]
 temperaturas = [
     [
-        [
-            [28, 29, 30, 31, 28, 27, 29],
-            [29, 30, 30, 32, 31, 30, 29],
-            [27, 28, 28, 29, 30, 29, 28],
-            [30, 31, 32, 32, 31, 30, 30]
-        ],
-        [
-            [31, 32, 33, 32, 31, 30, 30],
-            [29, 28, 30, 31, 29, 30, 29],
-            [30, 31, 32, 31, 30, 29, 28],
-            [32, 33, 34, 33, 32, 31, 30]
-        ]
+        [28, 29, 30, 31, 28, 27, 29],
+        [29, 30, 30, 32, 31, 30, 29],
+        [27, 28, 28, 29, 30, 29, 28],
+        [30, 31, 32, 32, 31, 30, 30]
     ],
     [
-        [
-            [33, 34, 35, 34, 32, 31, 30],
-            [31, 30, 29, 28, 27, 28, 29],
-            [28, 29, 30, 31, 30, 29, 28],
-            [34, 35, 36, 35, 34, 33, 32]
-        ],
-        [
-            [36, 37, 36, 35, 34, 33, 32],
-            [32, 31, 30, 29, 28, 27, 28],
-            [30, 29, 31, 32, 32, 31, 30],
-            [37, 38, 39, 38, 37, 36, 35]
-        ]
+        [31, 32, 33, 32, 31, 30, 30],
+        [29, 28, 30, 31, 29, 30, 29],
+        [30, 31, 32, 31, 30, 29, 28],
+        [32, 33, 34, 33, 32, 31, 30]
     ],
     [
-        [
-            [29, 30, 30, 31, 29, 28, 29],
-            [28, 27, 26, 27, 28, 29, 30],
-            [30, 31, 32, 31, 30, 29, 28],
-            [31, 32, 33, 34, 33, 32, 31]
-        ],
-        [
-            [30, 31, 32, 31, 30, 29, 28],
-            [29, 28, 27, 28, 29, 30, 31],
-            [32, 33, 34, 35, 34, 33, 32],
-            [33, 34, 35, 36, 35, 34, 33]
-        ]
+        [33, 34, 35, 34, 32, 31, 30],
+        [31, 30, 29, 28, 27, 28, 29],
+        [28, 29, 30, 31, 30, 29, 28],
+        [34, 35, 36, 35, 34, 33, 32]
     ]
 ]
 
-# Paso 2: Calcular el promedio de temperaturas para cada ciudad y semana
-ciudades = len(temperaturas)
-semanas = len(temperaturas[0][0])
 
-for ciudad in range(ciudades):
-    for semana in range(semanas):
-        total_temperaturas = 0
-        dias = len(temperaturas[ciudad][0][semana])
-        for dia in range(dias):
-            total_temperaturas += sum(temperaturas[ciudad][d][semana][dia] for d in range(2))
-        promedio = total_temperaturas / (dias * 2)  # Promedio para 2 semanas
-        print(f"Promedio de temperaturas para Ciudad {ciudad + 1}, Semana {semana + 1}: {promedio:.2f}°C")
+# Función para calcular el promedio de temperaturas por ciudad
+def calcular_promedio_ciudad(temperaturas):
+    promedios_ciudades = []
 
+    try:
+        ciudades = len(temperaturas)
+        semanas = len(temperaturas[0])
+        dias_semana = len(temperaturas[0][0])
+
+        for ciudad in range(ciudades):
+            total_temperaturas_ciudad = 0
+            total_dias = 0
+
+            for semana in range(semanas):
+                for dia in range(dias_semana):
+                    total_temperaturas_ciudad += temperaturas[ciudad][semana][dia]
+                    total_dias += 1
+
+            promedio_ciudad = total_temperaturas_ciudad / total_dias
+            promedios_ciudades.append(promedio_ciudad)
+
+    except IndexError:
+        print("Error: La estructura de datos de temperaturas no es válida.")
+        return []
+
+    return promedios_ciudades
+
+
+# Ejemplo de uso de la función con tus datos
+promedios = calcular_promedio_ciudad(temperaturas)
+
+if promedios:
+    for i, promedio in enumerate(promedios):
+        print(f"Promedio de temperaturas para Ciudad {i + 1}: {promedio:.2f}°C")
+else:
+    print("No se pudieron calcular los promedios debido a un error en la estructura de datos.")
